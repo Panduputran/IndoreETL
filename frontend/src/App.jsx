7dc846a4e525ad3eb5f6013bd2b2398bc67df62d
aburizky <4buurizky@gmail.com>
2026-08-24 08:43:16 +0700

    Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ import { SidebarProvider } from './components/context/SidebarContext';
 
 // Import Pages
 import UploadBordero from './pages/UploadBordero';
-import FormFire from './pages/FormFire';
-import FormKredit from './pages/FormKredit';
+import FormFire from './pages/form/FormFire';
+import FormKredit from './pages/form/FormKredit';
 import MasterMapping from './pages/MasterMapping';
 import FormIpr from './pages/FormIpr';
 import Dashboard from './pages/Dashboard';
@@ -21,8 +21,8 @@ export default function App() {
           <Route element={<MainLayout />}>
             <Route path="/" element={<Navigate to="/upload" replace />} />
             <Route path="/upload" element={<UploadBordero />} />
-            <Route path="/form-fire" element={<FormFire />} />
-            <Route path="/form-kredit" element={<FormKredit />} />
+            <Route path="/form/form-fire" element={<FormFire />} />
+            <Route path="/form/form-kredit" element={<FormKredit />} />
             <Route path="/master/mapping" element={<MasterMapping />} />
             <Route path="/form-ipr" element={<FormIpr />} />
             <Route path="/dashboard" element={<Dashboard />} />
