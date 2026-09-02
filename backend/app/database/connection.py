276e6d77698977993db82f9292dc15cfcbd0d999
aburizky <4buurizky@gmail.com>
2026-09-02 18:55:15 +0700

    Fix connection

--- backend/app/database/connection.py
+++ backend/app/database/connection.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv('DB_USER', 'postgres')
-DB_PASSWORD = os.getenv('DB_PASSWORD', '')
-DB_HOST = os.getenv('DB_HOST', 'localhost')
-DB_PORT = os.getenv('DB_PORT', '5432')
-DB_NAME = os.getenv('DB_NAME', 'postgres')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
 
 def get_database_url() -> str:
     """
